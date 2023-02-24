36e79c27f95df552193b33e85d0ffc75f1762d2f
collinsolayemi <collinsolayemi@gmail.com>
2023-02-24 06:56:29 -0800

    implementing a user schema

---
+++ src/db/user.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+//creating a user schema
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+export const userModel = mongoose.model('User', UserSchema);
