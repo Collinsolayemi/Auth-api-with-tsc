1686795c80543920631b4b2aee2f85ac557edbc4
collinsolayemi <collinsolayemi@gmail.com>
2023-02-27 12:33:59 -0800

    updating the user

--- src/router/user.ts
+++ src/router/user.ts
@@ -4,6 +4,7 @@ import { isAuthenticated, isOwner } from '../middleware/index';
 
 export default (router: Router) => {
   router.get('/users', isAuthenticated, getAllUsers);
+
   router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
   router.patch('/user/:id', isAuthenticated, isOwner, updateUser);
 };
